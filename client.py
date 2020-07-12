4d97afcebc08669eefb09802c117f562421e2f48
d.rybalchenko <d.rybalchenko@visionlabs.ru>
2020-07-12 08:28:43 +0300

    add comments

--- client.py
+++ client.py
@@ -7,14 +7,16 @@ from tqdm import tqdm
 
 videoname = 'SIZ_2_Work_Area_View_13_2020-06-04_05%3A15%3A51.209912'
 response = requests.post('http://127.0.0.1:2299/get_video', json={'videoname': videoname})
-
-visualization = True
 response = response.json()
-decode_time = 0
+
+visualization, decode_time = True, 0
 for frame, frame_boxes in tqdm(list(zip(response['frames'], response['boxes']))):
     t = default_timer()
+    # utf-8 string to bytes
     frame = base64.decodebytes(frame.encode())
+    # bytes to encoded jpg img
     frame = np.frombuffer(frame, np.uint8)
+    # decode jpg image
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
     decode_time += default_timer() - t
 
