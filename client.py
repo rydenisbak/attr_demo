8d747822a47d6ee15fbd494b2d0cb7d694e673ac
d.rybalchenko <d.rybalchenko@visionlabs.ru>
2020-07-12 08:17:28 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import requests
+import base64
+from timeit import default_timer
+from tqdm import tqdm
+
+videoname = 'SIZ_2_Work_Area_View_13_2020-06-04_05%3A15%3A51.209912'
+response = requests.post('http://127.0.0.1:2299/get_video', json={'videoname': videoname})
+
+visualization = True
+response = response.json()
+decode_time = 0
+for frame, frame_boxes in tqdm(list(zip(response['frames'], response['boxes']))):
+    t = default_timer()
+    frame = base64.decodebytes(frame.encode())
+    frame = np.frombuffer(frame, np.uint8)
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    decode_time += default_timer() - t
+
+    # draw boxes and scores
+    if visualization:
+        for box in frame_boxes:
+            score = box.split(',')[-1]
+            x1, y1, x2, y2 = [int(s) for s in box.split(',')[:-1]]
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(frame, score, (x1, y1 - 5), 1, 1, (255, 0, 0), 2)
+
+        cv2.imshow(videoname, cv2.resize(frame, (0, 0), fx=1/3, fy=1/3))
+        cv2.waitKey(1)
+
+decode_time = decode_time * 1000 / len(response["frames"])
+fps = 1000 / decode_time
+print(f'average decode time: {decode_time} ms\nframes per second: {fps}')
+
---
+++ server.py
@@ -0,0 +1,42 @@
+from sanic import Sanic
+from sanic.response import HTTPResponse
+import ujson
+import cv2
+import pandas as pd
+from tqdm import tqdm
+import base64
+
+app = Sanic(__name__)
+
+
+@app.route('/get_video', methods=['POST'])
+async def get_video(request):
+    frames = []
+    boxes = []
+    videoname = request.json['videoname']
+
+    cap = cv2.VideoCapture(videoname + '.mp4')
+    anno = pd.read_csv(videoname + '.csv')
+
+    for i_frame, frame_data in tqdm(anno.groupby('i_frame'), desc=f'Read {videoname}'):
+        # read specific frame
+        if i_frame != cap.get(cv2.CAP_PROP_POS_FRAMES):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
+        _, frame = cap.read()
+
+        # encode to jpg bytes
+        frame_jpg = cv2.imencode('.jpg', frame)[1].tobytes()
+        # bytes to utf8 string
+        frames.append(base64.b64encode(frame_jpg).decode())
+
+        # read all boxes in the frame
+        frame_boxes = []
+        for idx in frame_data.index:
+            frame_boxes.append(frame_data.loc[idx].bbox)
+        boxes.append(frame_boxes)
+
+    return HTTPResponse(ujson.dumps({'frames': frames, 'boxes': boxes}), status=201)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=2299, debug=False)
