a3e2646a7301d8ec45365f4bc683dba8257ac8fe
d.rybalchenko <d.rybalchenko@visionlabs.ru>
2020-07-14 10:23:27 +0300

    send video

--- client.py
+++ client.py
@@ -1,7 +1,5 @@
-import numpy as np
 import cv2
 import requests
-import base64
 from timeit import default_timer
 from tqdm import tqdm
 
@@ -9,29 +7,27 @@ videoname = 'SIZ_2_Work_Area_View_13_2020-06-04_05%3A15%3A51.209912'
 response = requests.post('http://127.0.0.1:2299/get_video', json={'videoname': videoname})
 response = response.json()
 
+cap = cv2.VideoCapture(response['result_video'])
 visualization, decode_time = True, 0
-for frame, frame_boxes in tqdm(list(zip(response['frames'], response['boxes']))):
+for frame_boxes in tqdm(response['boxes']):
     t = default_timer()
-    # utf-8 string to bytes
-    frame = base64.decodebytes(frame.encode())
-    # bytes to encoded jpg img
-    frame = np.frombuffer(frame, np.uint8)
-    # decode jpg image
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    status, frame = cap.read()
+    if not status:
+        raise IOError(f'Video file {response["result_video"]} is broken')
     decode_time += default_timer() - t
 
     # draw boxes and scores
     if visualization:
         for box in frame_boxes:
             score = box.split(',')[-1]
-            x1, y1, x2, y2 = [int(s) for s in box.split(',')[:-1]]
+            x1, y1, x2, y2 = [int(float(s) * response['scale']) for s in box.split(',')[:-1]]
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
             cv2.putText(frame, score, (x1, y1 - 5), 1, 1, (255, 0, 0), 2)
 
-        cv2.imshow(videoname, cv2.resize(frame, (0, 0), fx=1/3, fy=1/3))
+        cv2.imshow(videoname, frame)
         cv2.waitKey(1)
 
-decode_time = decode_time * 1000 / len(response["frames"])
+decode_time = decode_time * 1000 / len(response["boxes"])
 fps = 1000 / decode_time
 print(f'average decode time: {decode_time} ms\nframes per second: {fps}')
 
--- server.py
+++ server.py
@@ -4,30 +4,36 @@ import ujson
 import cv2
 import pandas as pd
 from tqdm import tqdm
-import base64
+import numpy as np
+import uuid
 
 app = Sanic(__name__)
+TARGET_WIDTH = 1080
 
 
 @app.route('/get_video', methods=['POST'])
 async def get_video(request):
-    frames = []
     boxes = []
     videoname = request.json['videoname']
 
     cap = cv2.VideoCapture(videoname + '.mp4')
     anno = pd.read_csv(videoname + '.csv')
 
+    scale = TARGET_WIDTH / cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    target_height = int(np.round((cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * scale)))
+    fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    result_video = str(uuid.uuid4()) + '.mp4'
+
+    vw = cv2.VideoWriter(result_video, fourcc, fps, (TARGET_WIDTH, target_height))
+
     for i_frame, frame_data in tqdm(anno.groupby('i_frame'), desc=f'Read {videoname}'):
         # read specific frame
         if i_frame != cap.get(cv2.CAP_PROP_POS_FRAMES):
             cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
         _, frame = cap.read()
-
-        # encode to jpg bytes
-        frame_jpg = cv2.imencode('.jpg', frame)[1].tobytes()
-        # bytes to utf8 string
-        frames.append(base64.b64encode(frame_jpg).decode())
+        frame = cv2.resize(frame, (TARGET_WIDTH, target_height), interpolation=cv2.INTER_NEAREST)
+        vw.write(frame)
 
         # read all boxes in the frame
         frame_boxes = []
@@ -35,7 +41,10 @@ async def get_video(request):
             frame_boxes.append(frame_data.loc[idx].bbox)
         boxes.append(frame_boxes)
 
-    return HTTPResponse(ujson.dumps({'frames': frames, 'boxes': boxes}), status=201)
+    vw.release()
+    return HTTPResponse(ujson.dumps({'boxes': boxes,
+                                     'scale': scale,
+                                     'result_video': result_video}), status=201)
 
 
 if __name__ == '__main__':
